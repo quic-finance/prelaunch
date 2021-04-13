1ab4840ca945e91ea4f89d96b3027f87f8615e66
Mark Wagner <mark@quicsystems.com>
2021-04-13 10:23:42 -0500

    Site

--- hooks/useQuic.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { Context } from '../contexts/QuicProvider'
-
-const useQuic = () => {
-  const { quic } = useContext(Context)
-  return quic
-}
-
-export default useQuic
